8f676fc6477c2f536a8f6937f870fd7e8db51128
BGJ06 <rajmithun434@gmail.com>
2026-06-04 01:23:26 +0530

    Fix: Configure subprojects to override compileSdk using modern AGP 9.0+ DSL classes

--- tn_party_connect/android/build.gradle.kts
+++ tn_party_connect/android/build.gradle.kts
@@ -24,13 +24,13 @@ tasks.register<Delete>("clean") {
 }
 
 subprojects {
-    plugins.withId("com.android.library") {
-        extensions.configure<com.android.build.gradle.LibraryExtension> {
+    plugins.withId("com.android.application") {
+        extensions.configure<com.android.build.api.dsl.ApplicationExtension> {
             compileSdk = 36
         }
     }
-    plugins.withId("com.android.application") {
-        extensions.configure<com.android.build.gradle.AppExtension> {
+    plugins.withId("com.android.library") {
+        extensions.configure<com.android.build.api.dsl.LibraryExtension> {
             compileSdk = 36
         }
     }
